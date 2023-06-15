37017c70f25c8678d870ad565503c3815ed1e075
lavish <lavishjain36@gmail.com>
2023-06-15 21:55:44 +0530

    node file date time

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-//Importing the http module
-const http=require('http');
+const watch=require('./watches/digitalwatches');
 
-//Define the port number 
-const port=4000;//server will listen for incoming request
-
-//create a server using http.createServer()
-const server=http.createServer((req,resp)=>{
-    //Set the response header
-    // resp.statusCode=200;
-    // resp.setHeader('Content-Type','text/plain');
-    resp.writeHeader(200,{'Content-Type':'text/html'})
-    resp.write("Hello Everyone. Welcome to Node.js with Basic server.Server is Started.");
-    resp.end();
-
-})
-
-//Server gets Started and listen for incoming request
-server.listen(port,()=>{
-    console.log('Server Started '+ port);
-})
-
+//use set interval function to call the indianwatch after every second 
+setInterval(watch.indianWatch,1000);
